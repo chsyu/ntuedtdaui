f329c5ce40107f0ef5f48f20b92a7f46f0d70c24
chsyu <chsyu@tea.ntue.edu.tw>
2020-11-20 23:13:28 +0800

    remove cors on https

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors')
+// const cors = require('cors')
 const mongoose = require('mongoose');
 const productRouter = require("./routers/productRouter");
 const userRouter = require("./routers/userRouter");
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+// app.use(cors());
 
 app.use("/api/products", productRouter);
 app.use("/api/users", userRouter);
